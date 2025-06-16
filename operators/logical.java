b201f47fd0d6366e8d33d0924eab7363b5c44432
Lalithakonakalla <lalithakonakalla14@gmail.com>
2025-06-16 16:24:43 +0530

    Add files via upload

---
+++ operators/logical.java
@@ -0,0 +1,12 @@
+class logical
+//AND OR NOT
+{
+public static void main(String[]args)
+{
+int a=10,b=20;
+System.out.println(a<11 && b<1);
+System.out.println(a<11||b<2);
+System.out.println(a==b);
+
+}
+}
